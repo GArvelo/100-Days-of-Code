694e7b6effc6fcd8120eb5f5875bba2b0e5e9b34
Geraldo Arvelo <119348946+GArvelo@users.noreply.github.com>
2023-03-01 15:58:35 +0000

    Challenge Exercise 1  - Printing

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 # Write your code below this line 👇
 print("hello world!")
-      
+print("Day 1 - Python Print Function" "\n" 
+      "The function is declared like this:""\n" 
+      "print('what to print')")
+#In Python, the new line character “\n” is used to create a new line. When inserted in a string all the characters after the character are added to a new line. Essentially the occurrence of the “\n” indicates that the line ends here and the remaining characters would be displayed in a new line.
